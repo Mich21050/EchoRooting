9f8f98e04046aa69e3f9b5109dfdf4db8818e243
Mich21050 <m_wa@gmx.at>
2022-01-05 21:38:35 +0100

    moved system files to different repo

--- SystemFiles/usr/local/bin/isSaviour.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if (. /etc/ota_version && [ "$IS_SAVIOUR_OF_THE_UNIVERSE" = 'Y' ]) >/dev/null 2>&1; then
-  echo '1'
-  exit 0
-fi
-
-echo '0'
-exit 0
--- SystemFiles/usr/local/bin/reload-wifi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script reloads the WiFi driver module
-
-stop modules-atheros || :
-sleep 1
-start modules-atheros || :
